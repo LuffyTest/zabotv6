683cc317b47866adcbb4c8b2b72c2370cce43ee5
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-14 12:11:55 +0530

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -163,12 +163,15 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
       }
       global.opts['swonly'] = isEnable
       break
-   case 'simi':
-    if (m.isGroup) {
-      if (!(isAdmin || isOwner)) {
-      global.dfail('admin', m, conn)
-      throw 0
-     }
+        case 'simi':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn)
+          throw 0
+        }
+      }
+      chat.simi = isEnable
+      break
     default:
       if (!/[01]/.test(command)) return m.reply(`
 List option: welcome | delete | public | antilink | antidelete | antitoxic | autolevelup | detect | document | whitelistmycontacts | restrict | nyimak | autoread | pconly | gconly | swonly
