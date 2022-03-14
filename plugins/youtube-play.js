9953ecd8d85b9cf8f448733227b2d52bc7423a2e
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-14 16:19:31 +0530

    Update youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -2,22 +2,23 @@ import { youtubeSearch } from '@bochilteam/scraper'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
   if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
   let vid = (await youtubeSearch(text)).video[0]
-  if (!vid) throw 'Video/Audio Tidak ditemukan'
+  if (!vid) throw 'Video/Audio Not found'
   let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
   const url = 'https://www.youtube.com/watch?v=' + videoId
   await conn.sendHydrated(m.chat, `
-📌 *Title:* ${title}
+*🔮 Title:* ${title}
+🔻 *Published:* ${publishedTime}
+🍃 *Duration:* ${durationH}
+🎐 *Views:* ${viewH}
+
 🔗 *Url:* ${url}
-🖹 *Description:* ${description}
-⏲️ *Published:* ${publishedTime}
-⌚ *Duration:* ${durationH}
-👁️ *Views:* ${viewH}
+💫 *Description:* ${description}
   `.trim(), wm, thumbnail, '', '', '', '', [
     ['Audio', `${usedPrefix}yta ${url} yes`], 
     ['Video', `${usedPrefix}ytv ${url} yes`]
   ], m, {asLocation: 1})
 }
-handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
+handler.help = ['play', 'play2'].map(v => v + ' <name>')
 handler.tags = ['downloader']
 handler.command = /^play2?$/i
 
