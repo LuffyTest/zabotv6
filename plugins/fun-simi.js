069056a39c82570790903dbe64f5a6f785c49601
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-14 12:29:54 +0530

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,8 +1,9 @@
-import fetch from 'node-fetch'
-let handler = async (m, {text}) => {
-  let api = await fetch(`https://simsimi.info/api/?text=${text}&lc=id`)
-  let res = await api.json()
-  m.reply(res.success)
+let fetch = require('node-fetch')
+let handler = async (m, { text }) => {
+  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "en" }, ''))
+  let json = await res.json()
+  if (json.success) m.reply(json.success)
+  else throw json
 }
 handler.command = ['simi']
 handler.tags = ['fun']
