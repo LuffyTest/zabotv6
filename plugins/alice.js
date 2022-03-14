a1892a2379fc4b583b37f73b0bf61afef516d088
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-14 13:15:14 +0530

    Update alice.js

--- plugins/alice.js
+++ plugins/alice.js
@@ -1,10 +1,14 @@
-let handler = async (m, { conn }) => {
- 
-  conn.sendButtonImg(m.chat, 'https://telegra.ph/file/89292e86386a9e55baf69.jpg', 'Hello Honey How Can i Help You? 💞', '© Alice 🥀', '✨ 𝑀𝐸𝑁𝑈 ✨', '.menu', m)
-  
-}
+let handler = async (m, { conn, text }) => {
+    let yh = global.alice
+    let url = yh[Math.floor(Math.random() * yh.length)]
+    conn.sendButton(m.chat, 'Hello Honey How Can i Help You? 💞', '© Alice 🥀', '✨ 𝑀𝐸𝑁𝑈 ✨', '.menu', m)
+  }
 handler.help = ['alive']
 handler.tags = ['tools']
 handler.command = /^(alive)$/i
 
 module.exports = handler
+  
+  global.alice = [
+    "https://telegra.ph/file/89292e86386a9e55baf69.jpg",
+      ]
