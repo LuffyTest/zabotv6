1efa8fe54c846caabdd6e01da40185a657b1ad1c
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-14 15:06:50 +0530

    Update anime-waifu.js

--- plugins/anime-waifu.js
+++ plugins/anime-waifu.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     if (!res.ok) throw await res.text()
     let json = await res.json()
     if (!json.url) throw 'Error!'
-    conn.sendButton(m.chat, 'Waifu 🎋🎐', wm, json.url, [['𝒲𝒜𝐼𝐹𝒰', `${usedPrefix}waifu`]], m)
+    conn.sendButton(m.chat, 'Waifu 🎋🎐', wm, json.url, [['𝓦𝓐𝓘𝓕𝓤', `${usedPrefix}waifu`]], m)
 }
 handler.help = ['waifu']
 handler.tags = ['internet']
