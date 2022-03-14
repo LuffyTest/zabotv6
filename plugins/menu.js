cf97ee61b56974295c2da53ee17afb99423576b5
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-14 12:53:53 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -147,9 +147,8 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     const pp = await conn.profilePictureUrl(conn.user.jid, 'image').catch(_ => './src/avatar_contact.png')
     conn.sendHydrated(m.chat, text.trim(), wm, pp, 'https://github.com/BochilGaming/games-wabot', 'Github', null, null, [
-      ['Donate', '/donasi'],
+      ['Rules', '/donasi'],
       ['Speed', '/ping'],
-      ['Owner', '/owner']
     ], m, {asLocation: 1})
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
