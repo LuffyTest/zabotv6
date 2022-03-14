37d7e0c8314ac7cd551db6338fd447d22ec67a74
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-14 15:58:00 +0530

    Update fun-generate_purba.js

--- plugins/fun-generate_purba.js
+++ plugins/fun-generate_purba.js
@@ -2,7 +2,7 @@ function handler(m, { text }) {
     let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
     m.reply(teks.replace(/[aiueo]/gi, '$&ve'))
 }
-handler.help = ['purba <teks>']
+handler.help = ['purba <text>']
 handler.tags = ['fun']
 handler.command =  /^(purba)$/i
 
