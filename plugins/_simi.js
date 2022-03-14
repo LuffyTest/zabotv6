2e71f8bce7886bab4edd25a0dd87f980bf15093a
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-14 17:20:15 +0530

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -1,21 +1,18 @@
 let fetch = require('node-fetch')
-
 let handler = m => m
 
-handler.before = async (m, { conn }) => {
-    let chat = db.data.chats[m.chat]
-    if (chat.simi && !chat.isBanned) {
-        if (/^.*false|disable|(turn)?off|0/i.test(m.text)) return
+handler.before = async (m) => {
+    let chat = global.db.data.chats[m.chat]
+    if (chat.simi && !chat.isBanned && !m.isGroup) {
+        if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
-        let res = await fetch(API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: 'en' }))
-        if (!res.ok) return m.reply(error)
+        let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: "id" }, ''))
+        if (!res.ok) throw 'maaf simi lagi sibuk'
         let json = await res.json()
-        if (m.quoted && m.quoted.sender === conn.user.jid) {
-         if (json.success == "panget si nica") return m.reply ("BAKA !")
-         m.reply(json.success)
+        if (json.success == 'lu ngomong apa sih') await m.reply('gapaham:v')
+        await m.reply(`${json.success}`)
         return !0
-      }
     }
-    return !0
+    return true
 }
-export default handler
+module.exports = handler
