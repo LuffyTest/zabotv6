fd5065170b20a7cf4bfcf899f43a5b741465e908
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-14 16:36:52 +0530

    Update group-settings.js

--- plugins/group-settings.js
+++ plugins/group-settings.js
@@ -1,30 +1,19 @@
-let { GroupSettingChange } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-	let isClose = {
-		'open': false,
-		'buka': false,
-		'on': false,
-		'1': false,
-		'close': true,
-		'tutup': true,
-		'off': true,
-		'0': true,
-	}[(args[0] || '')]
-	if (isClose === undefined) {
-		await conn.send2Button(m.chat, `
-Use:
-${usedPrefix + command} <open/close>
-Example:
-${usedPrefix + command} open
-${usedPrefix + command} close
-	`.trim(), '© Rias Gremory', 'OPEN', ',grup 1', 'CLOSE', ',grup 0', m)
-		throw 0
-	}
-	await conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, isClose)
+    let isClose = { // Switch Case Like :v
+        'open': 'not_announcement',
+        'close': 'announcement',
+    }[(args[0] || '')]
+    if (isClose === undefined)
+        throw `
+*Incorrect format! Example :*
+  *○ ${usedPrefix + command} close*
+  *○ ${usedPrefix + command} open*
+`.trim()
+    await conn.groupSettingUpdate(m.chat, isClose)
 }
-handler.help = ['group <open/close>']
+handler.help = ['group *open / close*']
 handler.tags = ['group']
-handler.command = /^(gro?up)$/i
+handler.command = /^(group)$/i
 
 handler.admin = true
 handler.botAdmin = true
